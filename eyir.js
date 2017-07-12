3d22ab364b635a98936e30b4764d9a29a8d90e25
Marok <adamhl44@gmail.com>
2017-07-11 21:17:14 -0400

    Add edit and delete message embeds

--- eyir.js
+++ eyir.js
@@ -84,6 +84,19 @@ bot.on("guildMemberRemove", member => {
   memberLeftEmbed(member);
 });
 
+bot.on("messageUpdate", (oldMessage, newMessage) => {
+
+  if (!oldMessage.content && !newMessage.content) {
+    return;
+  }
+
+  messageEditEmbed(oldMessage, newMessage);
+});
+
+bot.on("messageDelete", message => {
+  messageDeleteEmbed(message);
+});
+
 function newMemberMessage(member) {
   
   const welcomeMessage = member.displayName + ", welcome to **Skyhold**! Please go over the server rules in #welcome. Before asking a question, go over all of the available guides and resources in #guides-resources-faq; many frequently asked questions are answered there. Remember to check the Pinned Messages in each text channel for additional information. You can do so by clicking the Pin icon at the top right of your Discord window: <http://i.imgur.com/TuYQkjJ.png>. If you're unable to find an answer to your question or if you need clarification on something, please ask! That's what we're here for. :smile: We hope you enjoy your time in Skyhold!"
@@ -137,6 +150,49 @@ function memberLeftEmbed(member) {
     .setFooter(`ID: ${memberUserID}`)
     .setTimestamp()
     .setColor('BLUE');
+
+  channelCache.getByName("server-log")[0].send({embed})
+      .catch(console.error);
+}
+
+function messageEditEmbed(oldMessage, newMessage) {
+
+  const member = newMessage.member;
+
+  const memberUser = member.user.tag;
+  const memberUserID = member.user.id;
+  const memberUserAvatarURL = member.user.displayAvatarURL;
+
+  let embed = new Discord.RichEmbed()
+    .setAuthor("Message Edited", memberUserAvatarURL)
+    .setTitle(memberUser)
+    .setDescription(`<@${memberUserID}>`)
+    .setFooter(`ID: ${memberUserID}`)
+    .setTimestamp()
+    .setColor('ORANGE')
+    .addField("Original Message", oldMessage.content, false)
+    .addField("After Edit", newMessage.content, false);
+
+  channelCache.getByName("server-log")[0].send({embed})
+      .catch(console.error);
+}
+
+function messageDeleteEmbed(message) {
+
+  const member = message.member;
+
+  const memberUser = member.user.tag;
+  const memberUserID = member.user.id;
+  const memberUserAvatarURL = member.user.displayAvatarURL;
+
+  let embed = new Discord.RichEmbed()
+    .setAuthor("Message Deleted", memberUserAvatarURL)
+    .setTitle(memberUser)
+    .setDescription(`<@${memberUserID}>`)
+    .setFooter(`ID: ${memberUserID}`)
+    .setTimestamp()
+    .setColor('RED')
+    .addField("Message", message.content, false);
 
   channelCache.getByName("server-log")[0].send({embed})
       .catch(console.error);
